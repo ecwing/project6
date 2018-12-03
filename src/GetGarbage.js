4c1789523a038eb5ed04a5eb5208d08d95278d59
ecwing <ericcwing@gmail.com>
2018-12-03 18:39:29 -0500

    search bar WIP

--- src/GetGarbage.js
+++ src/GetGarbage.js
@@ -6,7 +6,8 @@ class Api extends Component {
     constructor() {
         super()
         this.state = {
-            returnInfo: []
+            returnInfo: [],
+            search: "" 
         }
     }
     componentDidMount() {
@@ -15,21 +16,36 @@ class Api extends Component {
 
     getGarbage = () => {
         axios.get(" https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000", {
-            // params: {
-            //     keywords: []
-            // }
+            params: {
+                description: this.state.keyword
+            }
         })
         .then(res => {
             console.log('show us data', res)
             this.setState({
-                returnInfo: res
+                returnInfo: res.data
             })
         })
     }
+
+    updateSearch = (e) => {
+        this.setState({
+            search: e.target.value
+        })
+        console.log('test', this.state.search)
+    }
+
     render() {
+        let searchInfo = this.returnInfo
+        searchInfo = searchInfo.filter((info) => {
+            return info[1].keyword.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+        })
         return (
             <div>
                 <p>testing</p>
+                <input type="text"
+                    value={this.state.search}
+                    onChange={this.updateSearch.bind(this)} />
             </div>
         )
     }
