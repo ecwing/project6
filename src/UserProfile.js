4ccf1b63569bca13f8d2c7c7025a0677a4d11cdd
lucamorellato <luca.morellato@gmail.com>
2018-12-06 19:40:09 -0500

    have router sort of working

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, NavLink, Link} from "react-router-dom";
 import firebase from "firebase";
-
 
 
 class UserProfile extends Component {
    constructor(){
       super()
-      state = {
-
+      this.state = {
+         form: {}
       }
    }
-   render(){}
+
+
+   
+   	render(){
+      return(
+         <div>
+            <h1>POOOOOOOOO</h1>
+         </div>
+      )
+   }
 }
-export default Api;
+export default UserProfile;
