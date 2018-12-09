0637f7e055241d8d21958651bc2575b2be2b9778
ecwing <ericcwing@gmail.com>
2018-12-08 21:46:07 -0500

    Intial daily tip additions - two files 1 for array of tips and another main tips component :rocket:

---
+++ src/tips.js
@@ -0,0 +1,83 @@
+//these were taken from https://www.toronto.ca/services-payments/recycling-organics-garbage/houses/set-out-tips/
+
+//not finalized, can add more or change as we see fit
+
+const tips = [
+    // =================================
+    // Proper Waste Set Out Instructions
+    // =================================
+
+    {
+        title: 'Proper Waste Set Out Instructions',
+        description: "Place bins/items at curbside before 7 a.m.",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9646-Garbage-Set-Out.jpg'
+    },
+
+    {
+        title: 'Proper Waste Set Out Instructions',
+        description: "Leave space around all bins and items so the trucks’ automated arms can grab and lift bins, ensuring 0.5 metres in-between and that they are as close to the street as possible, clearly visible and accessible",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9646-Garbage-Set-Out.jpg'
+    },
+    // =====================
+    // GREEN BIN SET UP TIPS
+    // =====================
+    {
+        title: 'Proper Green bin Set Out Tips',
+        description: "With the dial/latch in the locked position, set the bin out for collection with the arrows on top facing the street",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9928-Green-Bin-Set-Out.png'
+    },
+    {
+        title: 'Proper Green bin Set Out Tips',
+        description: "The bin must be set out squarely facing the street so that the truck’s automated collection arms can grip the bin to lift, tilt and empty",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9928-Green-Bin-Set-Out.png'
+    },
+    {
+        title: 'Proper Green bin Set Out Tips',
+        description: "Do not put yard waste in the Green Bin. Yard waste is collected and processed separately.",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9928-Green-Bin-Set-Out.png'
+    },
+    // =====================
+    // BLUE BIN SET OUT TIPS
+    // =====================
+    {
+        title: 'Proper Blue bin Set Out Tips',
+        description: "If you have overflow recycling, put it in a clear bag and place it beside your bin",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9190-colour-set-out.jpg'
+    },       
+    {
+        title: 'Proper Blue bin Set Out Tips',
+        description: "If you regularly have overflow recycling you need to upsize your bin by calling 311",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9190-colour-set-out.jpg'
+    }, 
+    {
+        title: 'Proper Blue bin Set Out Tips',
+        description: "Put items in the bin loosely",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9190-colour-set-out.jpg'
+    },         
+    {
+        title: 'Proper Blue bin Set Out Tips',
+        description: "Wash bins regularly with dish soap and water",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9190-colour-set-out.jpg'
+    },
+    // ========================
+    // GARBAGE BIN SET OUT TIPS
+    // ========================
+    {
+        title: 'Proper Blue bin Set Out Tips',
+        description: "If you have garbage beyond what fits in your bin with the lid closed, it must be bagged, tagged and set out beside your Garbage Bin for collection.",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9190-colour-set-out.jpg'
+    },
+    {
+        title: 'Proper Blue bin Set Out Tips',
+        description: "The City will pick up oversized items (appliances, furniture, carpets, etc.) on your garbage collection day for a fee.",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9190-colour-set-out.jpg'
+    },
+    {
+        title: 'Proper Blue bin Set Out Tips',
+        description: "Wash bins regularly with dish soap and water",
+        picture: 'https://www.toronto.ca/wp-content/uploads/2017/10/9190-colour-set-out.jpg'
+    }
+    ];
+
+    
+export default tips;
